2921f4ce0876c6ca5b9072745a887c879c1e7646
ffsala2002-a11y <ffsala2002@gmail.com>
2026-02-25 23:25:58 -0300

    Update carrinho.js

--- src/js/carrinho.js
+++ src/js/carrinho.js
@@ -1,126 +1,295 @@
 import { fmt } from './util.js';
 
+
+
 export let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
 
+
+
 export function salvarCarrinho() {
+
   localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
 }
+
+
 
 export function adicionarCarrinho(produto) {
-  const produtoExistente = carrinho.find(p => p.nce === produto.nce);
+
+  
+
+  // garante padrão vindo do parser
+
+  const produtoPadrao = {
+
+    descricao: produto.descricao || "",
+
+    nce: produto.nce || "",
+
+    grupo: produto.grupo || "",
+
+    saldo: Number(produto.saldo || 0),
+
+    cor: produto.cor || "",
+
+    preco: Number(produto.preco) || 0
+
+  };
+
+  
+
+  const produtoExistente = carrinho.find(p => p.nce === produtoPadrao.nce);
+
+  
 
   if (produtoExistente) {
+
     produtoExistente.quantidade += 1;
+
   } else {
+
     carrinho.push({
-      ...produto,
+
+      ...produtoPadrao,
+
       quantidade: 1,
+
       garantia: 0
+
     });
+
   }
 
+  
+
   salvarCarrinho();
+
   render();
+
 }
+
+
 
 export function limparCarrinho() {
+
   carrinho.length = 0;
+
   salvarCarrinho();
+
   render();
+
 }
 
+
+
 export function render() {
+
   const lista = document.getElementById('lista');
+
   const resultado = document.getElementById('resultado');
+
+  
 
   if (!lista || !resultado) return;
 
+  
+
   resultado.style.display = carrinho.length ? 'none' : 'none';
+
   lista.innerHTML = '';
+
+  
 
   const garantias = JSON.parse(localStorage.getItem('garantias') || '[]');
 
+  
+
   carrinho.forEach((p, index) => {
+
+    console.log("PRECO NO CARRINHO:", p.preco);
+
     const div = document.createElement('div');
+
     div.classList.add('item');
 
+    
+
     const g = garantias.find(k => k.nce === p.nce);
+
     const valorG1 = g ? (g.g1 || 0) * p.quantidade : 0;
+
     const valorG2 = g ? (g.g2 || 0) * p.quantidade : 0;
 
+    
+
     div.innerHTML = `
-      <div>
-        <div class="descricao">
-        <strong class="nce">${p.nce}</strong>
-        <small>${p.descricao}</small>
-        </div>
 
-        <div class="box-quantidade">
-          <div class="quantidade">
-            Quantidade: <span>${p.quantidade}</span>
-          </div>
-          <div class="buttons">
-            <button class="btn-minus">−</button>
-            <button class="btn-plus">+</button>
-          </div>
-        </div>
 
-        <div class="garantia-item">
-          <label>Garantia:</label>
-          <select class="select-garantia">
-            <option value="0">Sem garantia</option>
-            <option value="1">GE 1 (${fmt(valorG1)})</option>
-            <option value="2">GE 2 (${fmt(valorG2)})</option>
-          </select>
-        </div>
+
+  <div>
+
+
+
+    <div class="box-descricao">
+
+      <p class="descricao">${p.descricao}</p>
+
+    </div>
+
+
+
+    <div class="info-produto">
+
+    <small class="nce">Nce: ${p.nce}</small>
+
+      <small>Grupo: ${p.grupo || "-"}</small>
+
+      <small>Cor: ${p.cor || "-"}</small>
+
+      <small>Saldo: ${p.saldo ?? "-"}</small>
+
+    </div>
+
+
+
+    <div class="box-quantidade">
+
+      <div class="quantidade">
+
+        Quantidade: <span>${p.quantidade}</span>
+
       </div>
 
-      <div>
-        <strong class="valor-total">
-          ${(p.preco * p.quantidade).toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-          })}
-        </strong>
+
+
+      <div class="buttons">
+
+        <button class="btn-minus">−</button>
+
+        <button class="btn-plus">+</button>
+
       </div>
-    `;
+
+    </div>
+
+
+
+    <div class="garantia-item">
+
+      <label>Garantia:</label>
+
+      <select class="select-garantia">
+
+        <option value="0">Sem garantia</option>
+
+        <option value="1">GE 1 (${fmt(valorG1)})</option>
+
+        <option value="2">GE 2 (${fmt(valorG2)})</option>
+
+      </select>
+
+    </div>
+
+
+
+  </div>
+
+
+
+  <div>
+
+    <strong class="valor-total">
+
+     ${(Number(p.preco || 0) * Number(p.quantidade || 0)).toLocaleString('pt-BR', {
+
+  style: 'currency',
+
+  currency: 'BRL'
+
+})}
+
+    </strong>
+
+  </div>
+
+`;
+
+    
 
     div.querySelector('.btn-plus').onclick = () => {
+
       p.quantidade++;
+
       salvarCarrinho();
+
       render();
+
     };
+
+    
 
     div.querySelector('.btn-minus').onclick = () => {
+
       if (p.quantidade > 1) p.quantidade--;
+
       else carrinho.splice(index, 1);
+
       salvarCarrinho();
+
       render();
+
     };
+
+    
 
     const select = div.querySelector('.select-garantia');
+
     select.value = p.garantia;
+
     select.onchange = e => {
+
       p.garantia = Number(e.target.value);
+
       salvarCarrinho();
+
       render();
+
     };
+
+    
 
     //BOTÃO APAGAR
+
     const btnApagar = document.createElement('button');
+
     btnApagar.textContent = 'Apagar';
+
     btnApagar.className = 'btn-apagar';
+
     btnApagar.innerHTML = `<img src="./src/img/trash-can.png" alt="Imagem logo lixeira">`
+
     btnApagar.onclick = () => {
+
       carrinho = carrinho.filter(item => item.nce !== p.nce);
+
       salvarCarrinho();
+
       render();
+
     };
 
+    
+
     div.appendChild(btnApagar);
+
     lista.appendChild(div);
+
   });
+
 }
+
+
+
 
 
 render();
